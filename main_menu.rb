c407dd11e251cf08494f08d26d4d01d5d37cc1e5
syncopation101 <bobby.cool00763@gmail.com>
2018-06-18 22:54:16 +0700

    Main Menu (under development, not objective yet)

--- main_menu.rb
+++ main_menu.rb
@@ -1,3 +1,6 @@
+#MAIN MENU
+
+#splash screen
 puts "               WELCOME TO                  "
 puts "=========================================== "
 puts "██████╗  ██████╗        ██████╗ ██╗     ██╗"
@@ -7,4 +10,25 @@ puts "██║   ██║██║   ██║╚════╝██║     
 puts "╚██████╔╝╚██████╔╝      ╚██████╗███████╗██║"
 puts "╚═════╝  ╚═════╝        ╚═════╝╚══════╝╚═╝"
 puts "=========================================== "
-#art by: http://patorjk.com/software/taag/#p=display&h=0&v=2&f=ANSI%20Shadow&t=GO-CLI                                         
+#art by: http://patorjk.com/software/taag/#p=display&h=0&v=2&f=ANSI%20Shadow&t=GO-CLI 
+
+#help screen
+puts "\n                         GO-CLI's guide for user"
+puts "\nshow map -- shows user's and drivers' location"
+puts "order go ride -- allocates nearest driver to give user a lift"
+puts "view history -- shows all trip user had made"
+puts "\n----------------------------------------------------------------------------"
+
+#user's input of command line
+print "\nPlease enter the command: "
+command = gets.chomp
+
+#checking user's input
+if command == "view history"
+	
+elsif command == "show map"
+	
+elsif command == "order go ride"
+
+else puts "Invalid command. Please try again"
+end                                        
