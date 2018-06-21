4bd844836f123990508b8cd85ee1ab8decdfc1c0
syncopation101 <bobby.cool00763@gmail.com>
2018-06-21 21:10:59 +0700

    GO-CLI is complete

--- GO-CLI.rb
+++ GO-CLI.rb
@@ -1,19 +1,19 @@
 #MAIN MENU
 
 #splash screen
-puts "                                WELCOME TO                  "
-puts "               =========================================== "
-puts "               ██████╗  ██████╗        ██████╗ ██╗     ██╗"
-puts "               ██╔════╝ ██╔═══██╗      ██╔════╝██║     ██║"
-puts "               ██║  ███╗██║   ██║█████╗██║     ██║     ██║"
-puts "               ██║   ██║██║   ██║╚════╝██║     ██║     ██║"
-puts "               ╚██████╔╝╚██████╔╝      ╚██████╗███████╗██║"
-puts "                ╚═════╝  ╚═════╝        ╚═════╝╚══════╝╚═╝"
-puts "               =========================================== "
+puts "                                 WELCOME TO                  "
+puts "                  =========================================== "
+puts "                  ██████╗  ██████╗        ██████╗ ██╗     ██╗"
+puts "                  ██╔════╝ ██╔═══██╗      ██╔════╝██║     ██║"
+puts "                  ██║  ███╗██║   ██║█████╗██║     ██║     ██║"
+puts "                  ██║   ██║██║   ██║╚════╝██║     ██║     ██║"
+puts "                  ╚██████╔╝╚██████╔╝      ╚██████╗███████╗██║"
+puts "                   ╚═════╝  ╚═════╝        ╚═════╝╚══════╝╚═╝"
+puts "                  =========================================== "
 #art by: http://patorjk.com/software/taag/#p=display&h=0&v=2&f=ANSI%20Shadow&t=GO-CLI 
 
 #help screen
-puts "\n                         type 'help' if you need more info"
+puts "\n                       type 'help' if you need more info"
 
 #user's input of command line
 require_relative 'classes & modules/input.rb'
